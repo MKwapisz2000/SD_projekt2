628310603980b94bb1d0ad38c497fe4cf7afb182
MichalskaMagda <162743346+MichalskaMagda@users.noreply.github.com>
2024-04-24 00:02:25 +0200

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,25 +1,36 @@
-#include <iostream> 
-#include "LinkedList.h" 
+#include "LinkedList.h"
+#include <iostream>
 
 using namespace std;
 
-// Destruktor
-LinkedList::~LinkedList() {
-    clear(); // Wywołanie metody clear() aby zwolnić pamięć
+// Konstruktor dla elementu listy (Nod)
+Nod::Nod(int d, int prio) : data(d), priority(prio), next(nullptr) {}
+
+// Implementacja konstruktora LinkedList
+LinkedList::LinkedList() : head(nullptr) {}
+
+// Implementacja destruktora
+LinkedList::~LinkedList() 
+{
+    clear(); // Wywołanie metody clear() aby zwolnić pamięć po obiekcie LinkedList
 }
 
 // Metoda dodająca element o priorytecie p
-// Jeśli elementy mają ten sam priorytet, stosowana jest zasada FIFO (First In, First Out)
-void LinkedList::insert(int e, int p) {
+void LinkedList::insert(int e, int p)
+{
     Nod* newNode = new Nod(e, p); // Utworzenie nowego węzła
     // Sprawdzenie czy lista jest pusta lub czy nowy element ma najwyższy priorytet
-    if (!head || p > head->priority) {
+    if (!head || p > head->priority) 
+    {
         newNode->next = head; // Nowy element staje się głową listy
         head = newNode;
-    } else {
+    } 
+    else 
+    {
         Nod* curr = head;
         // Szukanie miejsca dla nowego elementu w liście
-        while (curr->next && curr->next->priority >= p) {
+        while (curr->next && curr->next->priority >= p) 
+        {
             curr = curr->next;
         }
         newNode->next = curr->next; // Wstawienie nowego elementu
@@ -27,10 +38,12 @@ void LinkedList::insert(int e, int p) {
     }
 }
 
-// Metoda usuwająca element o najwyższym priorytecie
-void LinkedList::extract_max() {
-    if (!head) {
-        cout << "Lista jest pusta!" << endl; // Komunikat o błędzie
+// Implementacja metody extract_max
+void LinkedList::extract_max() 
+{
+    if (!head) 
+    {
+        cout << "Lista jest pusta!" << endl; // Komunikat o błędzie, jeśli lista jest pusta
         return;
     }
     Nod* temp = head;
@@ -38,78 +51,122 @@ void LinkedList::extract_max() {
     delete temp; // Zwolnienie pamięci zajmowanej przez usuwany element
 }
 
-// Metoda zwracająca (podejrzenie) element o najwyższym priorytecie
-int LinkedList::find_max() const {
-    if (!head) {
-        cout << "Lista jest pusta!" << endl; // Komunikat o błędzie
-        return -1; // Wartość specjalna w przypadku pustej listy
+// Implementacja metody find_max
+// Jeśli elementy mają ten sam priorytet, stosowana jest zasada FIFO (First In, First Out)
+void LinkedList::find_max() const 
+{
+    if (!head) 
+    {
+        cout << "Lista jest pusta!" << endl; // Komunikat o błędzie, jeśli lista jest pusta
+        return;
     }
-    return head->data; // Zwrócenie wartości pierwszego elementu
+
+    // Wyświetlenie informacji o elemencie o najwyższym priorytecie
+    cout << "Maksymalny priorytet znajduje sie na poczatku listy i wynosi: ";
+    cout << head->priority << ", a wartosc elementu to: " << head->data << endl;
+
+    int j = 0;
+    Nod* curr = head->next;
+    // Sprawdzenie czy istnieją inne elementy o tym samym priorytecie
+    while (curr && curr->priority == head->priority) 
+    {
+        // Wyświetlenie informacji o innych elementach o tym samym priorytecie
+        cout << "Inny element o tym samym priorytecie to: ";
+        cout << curr->priority << ", a wartosc elementu to: " << curr->data << endl;
+        curr = curr->next;
+        j++;
+    }
+
+    // Wyświetlenie komunikatu, jeśli nie ma innych elementów o tym samym priorytecie
+    if (j == 0) 
+    {
+        cout << "Brak innych elementow o tym samym priorytecie" << endl;
+    }
 }
 
-// Metoda modyfikująca priorytet elementu o danej wartości
-void LinkedList::modify_key(int e, int p) {
+// Implementacja metody modify_key
+void LinkedList::modify_key(int e, int p) 
+{
     Nod* prev = nullptr;
     Nod* curr = head;
+    int index = 0; // Zmienna przechowująca aktualny indeks
 
     // Szukanie elementu o danej wartości
-    while (curr && curr->data != e) {
+    while (curr && curr->data != e) 
+    {
         prev = curr;
         curr = curr->next;
+        index++;
     }
 
     // Jeśli nie znaleziono elementu, wyświetl komunikat o błędzie
-    if (!curr) {
-        cout << "Element o wartości " << e << " nie istnieje na liście." << endl;
+    if (!curr) 
+    {
+        cout << "Element o wartosci " << e << " nie istnieje na liscie." << endl;
         return;
     }
 
     // Jeśli element ma być przesunięty do przodu, usuń go z obecnej pozycji
-    if (p > curr->priority) {
-        if (prev) {
+    if (p > curr->priority) 
+    {
+        if (prev) 
+        {
             prev->next = curr->next; // Usunięcie elementu z listy
-        } else {
+        } 
+        else 
+        {
             head = curr->next; // Aktualizacja głowy listy, jeśli usuwany element był na początku
         }
 
         // Przeszukiwanie listy w celu znalezienia miejsca do wstawienia elementu
         Nod* temp = head;
         Nod* prev_temp = nullptr;
-        while (temp && temp->priority >= p) {
+        while (temp && temp->priority >= p) 
+        {
             prev_temp = temp;
             temp = temp->next;
         }
 
         // Wstawienie elementu na odpowiednie miejsce w liście
-        if (prev_temp) {
+        if (prev_temp) 
+        {
             curr->next = prev_temp->next;
             prev_temp->next = curr;
-        } else {
+        }
+        else 
+        {
             curr->next = head;
             head = curr;
         }
     }
     // Jeśli element ma być przesunięty do tyłu, usuń go z obecnej pozycji
-    else if (p < curr->priority) {
+    else if (p < curr->priority) 
+    {
         if (prev) {
             prev->next = curr->next; // Usunięcie elementu z listy
-        } else {
+        } 
+        else 
+        {
             head = curr->next; // Aktualizacja głowy listy, jeśli usuwany element był na początku
         }
 
         // Przeszukiwanie listy w celu znalezienia miejsca do wstawienia elementu
         Nod* temp = head;
         Nod* prev_temp = nullptr;
-        while (temp && temp->priority < p) {
+        while (temp && temp->priority < p) 
+        {
             prev_temp = temp;
             temp = temp->next;
         }
 
         // Wstawienie elementu na odpowiednie miejsce w liście
-        if (prev_temp) {
+        if (prev_temp) 
+        {
             curr->next = prev_temp->next;
             prev_temp->next = curr;
-        } else {
+        } 
+        else 
+        {
             curr->next = head;
             head = curr;
         }
@@ -117,25 +174,31 @@ void LinkedList::modify_key(int e, int p) {
 
     // Aktualizacja priorytetu elementu
     curr->priority = p;
+
+    // Wyświetlenie informacji o indeksie zmienionego priorytetu
+    cout << "Zmieniono priorytet elementu o wartosci " << e << " pod indeksem " << index << endl;
 }
 
-
-/// Metoda zwiększająca priorytet elementu o danym indeksie
-void LinkedList::increase_key(int index, int p) {
-    if (index < 0 || index >= return_size()) {
+// Implementacja metody increase_key
+void LinkedList::increase_key(int index, int p) 
+{
+    if (index < 0 || index >= return_size()) 
+    {
         cout << "Indeks poza zakresem!" << endl; // Komunikat o błędzie
         return;
     }
 
     Nod* curr = head;
     // Przesunięcie do elementu o danym indeksie
-    for (int i = 0; i < index; i++) {
+    for (int i = 0; i < index; i++) 
+    {
         curr = curr->next;
     }
 
     // Jeśli nowy priorytet jest mniejszy lub równy obecnemu, zwróć błąd
-    if (p <= curr->priority) {
-        cout << "Nowy priorytet musi być większy niż obecny!" << endl;
+    if (p <= curr->priority) 
+    {
+        cout << "Nowy priorytet musi byc wiekszy niz obecny!" << endl;
         return;
     }
 
@@ -143,7 +206,8 @@ void LinkedList::increase_key(int index, int p) {
     curr->priority = p;
 
     // Przesuń element w górę kolejki, jeśli to konieczne
-    while (curr->next && curr->next->priority < p) {
+    while (curr->next && curr->next->priority < p) 
+    {
         Nod* temp = curr->next;
         curr->next = temp->next;
         temp->next = curr;
@@ -151,22 +215,26 @@ void LinkedList::increase_key(int index, int p) {
     }
 }
 
-// Metoda zmniejszająca priorytet elementu o danym indeksie
-void LinkedList::decrease_key(int index, int p) {
-    if (index < 0 || index >= return_size()) {
+// Implementacja metody decrease_key
+void LinkedList::decrease_key(int index, int p) 
+{
+    if (index < 0 || index >= return_size()) 
+    {
         cout << "Indeks poza zakresem!" << endl; // Komunikat o błędzie
         return;
     }
 
     Nod* curr = head;
     // Przesunięcie do elementu o danym indeksie
-    for (int i = 0; i < index; i++) {
+    for (int i = 0; i < index; i++) 
+    {
         curr = curr->next;
     }
 
     // Jeśli nowy priorytet jest większy lub równy obecnemu, zwróć błąd
-    if (p >= curr->priority) {
-        cout << "Nowy priorytet musi być mniejszy niż obecny!" << endl;
+    if (p >= curr->priority) 
+    {
+        cout << "Nowy priorytet musi byc mniejszy niz obecny!" << endl;
         return;
     }
 
@@ -174,7 +242,8 @@ void LinkedList::decrease_key(int index, int p) {
     curr->priority = p;
 
     // Przesuń element w dół kolejki, jeśli to konieczne
-    while (curr->next && curr->next->priority > p) {
+    while (curr->next && curr->next->priority > p) 
+    {
         Nod* temp = curr->next;
         curr->next = temp->next;
         temp->next = curr;
@@ -182,38 +251,42 @@ void LinkedList::decrease_key(int index, int p) {
     }
 }
 
-
-// Metoda zwracająca rozmiar listy
-int LinkedList::return_size() const {
+// Implementacja metody return_size
+int LinkedList::return_size() const 
+{
     int count = 0;
     Nod* curr = head;
     // Przechodzenie po liście i zliczanie elementów
-    while (curr) {
+    while (curr) 
+    {
         count++;
         curr = curr->next;
     }
     return count;
 }
 
-// Metoda wyświetlająca zawartość listy
-void LinkedList::show() const {
+// Implementacja metody show
+void LinkedList::show() const 
+{
     Nod* curr = head;
     cout << "POCZATEK" << endl;
     int i = 0;
     // Przechodzenie po liście i wyświetlanie wartości oraz priorytetów elementów
     while (curr) {
-        cout << i +1<< ".) " << curr->priority << endl;
+        cout << i + 1 << ".) " << curr->priority << endl;
         curr = curr->next;
         i++;
     }
     cout << "KONIEC" << endl;
 }
 
-// Metoda czyszcząca listę
-void LinkedList::clear() {
+// Implementacja metody clear
+void LinkedList::clear() 
+{
     while (head) {
         Nod* temp = head;
         head = head->next;
         delete temp; // Zwolnienie pamięci zajmowanej przez usuwany element
     }
 }
+
